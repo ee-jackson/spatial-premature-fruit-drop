97b59d49497a980b514f0f1251e87f7cd6de1701
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2020-10-08 14:05:02 +0100

    fixed issue of NAs
    use the largest measure of dbh for each individual tree

--- code/calculateR50.R
+++ code/calculateR50.R
@@ -15,25 +15,17 @@ lapply(file_names, load, environment())
 
 #cd "/Users/eleanorjackson/OneDrive - University of Reading/SpatialPatterns/code"
 
-bci.tree <- bind_rows(bci.tree1,bci.tree2,bci.tree3,bci.tree4,bci.tree5,bci.tree6,bci.tree7,bci.tree8, .id = "df")
+bci.tree <- bind_rows(bci.tree3,bci.tree4,bci.tree5,bci.tree6,bci.tree7,bci.tree8, .id = "df")
 
 # Dmax as mean of 6 the individuals with the highest dbh
 bci.tree %>%
-	group_by(sp) %>%
-	arrange(desc(dbh)) %>% 
-	slice(6) %>% 
-	summarise(dbh=mean(dbh)) %>%
-	mutate(R50 = dbh/2) %>%
+	group_by(treeID) %>%
+	slice_max(dbh, n = 1) %>% # use the largest dbh for each treeID
 	ungroup() %>%
+	group_by(sp) %>%
+	slice_max(dbh, n = 6) %>% 
+	summarise(dbh=mean(dbh, na.rm = TRUE)) %>%
+	mutate(R50 = dbh/2) %>%
 	rename(dmax="dbh") -> tree.max
-
-# Dmax = the dbh of the individual with the largest value
-bci.tree %>%
-	group_by(sp) %>%
-	slice(which.max(dbh)) %>%
-	mutate(R50 = dbh/2) %>%
-	ungroup() %>%
-	select("sp", "dbh", "R50") %>%
-	rename(dmax="dbh") -> tree.max1
 
 write.csv(tree.max, "../output/tables/R50.csv")
