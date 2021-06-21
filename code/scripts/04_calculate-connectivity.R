d94d4d34b85f0045d91ccd7678376de707909c70
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2021-06-21 14:18:48 +0100

    don't calculate NFI

--- code/scripts/04_calculate-connectivity.R
+++ code/scripts/04_calculate-connectivity.R
@@ -9,14 +9,14 @@ library("tidyverse"); theme_set(theme_bw(base_size = 8))
 library("rdist")
 library("parallel")
 
-######## LOAD DATA ########
+# Load data ---------------------------
 load(here:here("data", "clean", "trapData.RData"))
 load(here:here("data", "clean", "treeData.RData"))
 
 trapDat$year <- as.character(trapDat$year)
 
 
-####### CHOOSE SPECIES #########
+# Select species ---------------------------
 trapDat %>%
 	group_by(SP4, year) %>%
 	mutate(n_traps = length(unique(trap))) %>%
@@ -46,16 +46,14 @@ summary <- full_join(trapSummary, treeSummary, by= "SP4")
 subset(summary, median_trees >15 & median_traps >15) %>%
 	pull(SP4) -> sp.list # this gives 40 species
 
-###############################################################################
-## Calculate euclidean distances
-###############################################################################
+# Calculate euclidean distances ---------------------------
 
 calculate_dist <- function (species, yr) {
 	# subset tree data
 	bd <- dplyr::filter(bci, (bci[,"SP4"]==species) & (bci[,"year"]==yr))
 
 	# subset trap data
-	td <- dplyr::filter(trapDat, (trapDat[,"SP4"]==species) & 
+	td <- dplyr::filter(trapDat, (trapDat[,"SP4"]==species) &
 		(trapDat[,"year"]==yr) )
 
 	# create distance matrix using x and y co-ordinates
@@ -70,7 +68,7 @@ calculate_dist <- function (species, yr) {
 }
 
 # create lists of sp and year to pass to function
-year.list <- unique(bci[["year"]]) 
+year.list <- unique(bci[["year"]])
 
 # apply function to all pairwise combinations of year and species
 # will return list of dfs
@@ -78,45 +76,13 @@ all.dists <- outer(sp.list, year.list, FUN = Vectorize(calculate_dist))
 
 bci.dists <- dplyr::bind_rows(all.dists)
 
-###############################################################################
-## Conspecific neighbourhood fecundity index
-###############################################################################
-
-# function to calculate NFI
-calculate_NFI <- function (trap) {
-	dplyr::filter(bci.dists, bci.dists[,trap] <= 50) %>% # subset to only trees within a 50m radius of the trap
-	group_by(treeID, year) %>% 
-	mutate(a = dbh / eval(parse(text=trap))) %>% # divide dbh by dist
-	ungroup() %>%
-	dplyr::select(year, SP4, trap, a) %>%
-	group_by(year, SP4) %>%
-	summarise(trap = paste(trap), NFI = sum(a), .groups = "drop") # sum over all trees to = 1 value per trap
-}
-
-# create list of trap IDs to pass through the function
-trap.list <- unique(trapDat$trap)
-
-# apply function to each trap and parallelize
-NFIdat <- mclapply(trap.list, calculate_NFI, mc.cores = 4)
-
-# bind output into one dataframe
-NFIdat.b <- bind_rows(NFIdat)
-
-head(NFIdat.b) #take a look at NFI values
-
-# merge with trap data
-trapConnect <- left_join(NFIdat.b, trapDat,  
-	by = c("trap", "year", "SP4"))
-
-###############################################################################
-## Hanski's Connectivity index
-###############################################################################
+# Hanski's Connectivity index ---------------------------
 
 # function to calculate CI
 calculate_CI <- function (trap) {
 	drop_na(bci.dists, trap) %>%
-	group_by(treeID, year) %>% 
-	mutate(a = dbh * exp( (-0.02) * eval(parse(text=trap)) ) ) %>% 
+	group_by(treeID, year) %>%
+	mutate(a = dbh * exp( (-0.02) * eval(parse(text=trap)) ) ) %>%
 	ungroup() %>%
 	dplyr::select(year, SP4, trap, a) %>%
 	group_by(year, SP4) %>%
@@ -129,13 +95,11 @@ CIdat.b <- bind_rows(CIdat)
 
 head(CIdat.b) #take a look at CI values
 
-###############################################################################
-## Merge and save dataset
-###############################################################################
+# Merge and save dataset ---------------------------
 
 # merge together into one big dataset
 trapConnect <- left_join(trapConnect, CIdat.b, by = c("trap", "year", "SP4"))
 
 # save it
-save(trapConnect, sp.list, 
+save(trapConnect, sp.list,
 	file = here:here("data", "clean", "trapConnect.RData"))
