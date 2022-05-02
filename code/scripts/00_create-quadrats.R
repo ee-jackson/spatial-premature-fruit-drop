67c78f679cb57f394c960bb6b610e06eb7dce46b
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2022-05-02 15:25:57 +0100

    output as csv not RData

--- code/scripts/00_create-quadrats.R
+++ code/scripts/00_create-quadrats.R
@@ -5,39 +5,42 @@
 ## Desc: assign each trap to a 50m2 quadrat
 ## Date created: 2020-10-07
 
-library("tidyverse"); theme_set(theme_bw(base_size = 8))
-library("sf")
+library("tidyverse") # v 1.3.1
+library("sf") # v 1.0-3
+library("here") # v 1.0.1
+
+theme_set(theme_bw(base_size = 8))
 
 # co-ordinates of the original 200 traps
-trap_loc <- read.csv(here::here("data", "raw", "trapLocations.csv")) 
+trap_loc <- read.csv(here::here("data", "raw", "trapLocations.csv"))
 
 # paired traps - angles at 2m from the original traps
 pair_traps <- read.csv(here::here("data", "raw", "pairedTraps.csv"))
 
-# format trap ids
+# format trap IDs
 trap_loc$trap <- formatC(trap_loc$trap, width = 3, format = "d", flag = "0")
 trap_loc$trap <- paste("trap", trap_loc$trap, sep="_")
 
-pair_traps$trap <- formatC(pair_traps$trap, 
+pair_traps$trap <- formatC(pair_traps$trap,
 	width = 3, format = "d", flag = "0")
 pair_traps$trap <- paste("trap", pair_traps$trap, sep="_")
 
-pair_traps$paired_trap <- formatC(pair_traps$paired_trap, 
+pair_traps$paired_trap <- formatC(pair_traps$paired_trap,
 	width = 3, format = "d", flag = "0")
 pair_traps$paired_trap <- paste("trap", pair_traps$paired_trap, sep="_")
 
 # add coordinates from trap_loc to paired traps
 pair_traps %>%
-	left_join(trap_loc, by = "trap") %>% 
+	left_join(trap_loc, by = "trap") %>%
 	select(-trap) %>%
 	rename(trap = paired_trap) -> pair_traps_loc
 
 # first convert from degrees to radians
 # then calculate coordinates of paired traps
 # and then bind
-pair_traps_loc %>% 
+pair_traps_loc %>%
 	mutate(angle = angle * (pi/180)) %>%
-	mutate(X = X + cos(angle) * 2, Y = Y + sin(angle) * 2) %>% 
+	mutate(X = X + cos(angle) * 2, Y = Y + sin(angle) * 2) %>%
 	select(-angle) %>%
 	bind_rows(trap_loc) -> all_trap_loc
 
@@ -59,18 +62,18 @@ quads <- st_make_grid(
   flat_topped = FALSE)
 
 #convert polygons to sf object and add id column
-quads %>% 
-	st_sfc() %>% 
-	st_sf() %>% 
-	mutate(quadrat = paste(seq(1:200))) -> quads_sf 
+quads %>%
+	st_sfc() %>%
+	st_sf() %>%
+	mutate(quadrat = paste(seq(1:200))) -> quads_sf
 
 # calculate what traps are in what quadrats
 joined <- st_intersection(trap_sf, quads_sf)
 
 # plot to check
-ggplot() + 
+ggplot() +
 	geom_sf(data = quads_sf) +
-	geom_sf(data = joined, aes(col=quadrat)) 
+	geom_sf(data = joined, aes(col=quadrat))
 
 # convert to dataframe
 joined %>%
@@ -87,4 +90,7 @@ trap_loc_quad$quadrat <- paste("quadrat", trap_loc_quad$quadrat, sep="_")
 trap_loc_quad[duplicated(trap_loc_quad$trap),]
 trap_loc_quad <- trap_loc_quad[!duplicated(trap_loc_quad$trap),]
 
-save(trap_loc_quad, file = here:here("data", "clean", "trapLocations.RData"))
+trap_loc_quad %>%
+  arrange(trap) %>%
+  write.csv(file = here::here("data", "clean", "trap_locations.csv"),
+          row.names = FALSE)
