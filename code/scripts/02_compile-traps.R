26ba0ab207950de10aaef858a02e039c7d94adee
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2022-05-07 11:28:56 +0100

    update to estimate seeds using capsules

--- code/scripts/02_compile-traps.R
+++ code/scripts/02_compile-traps.R
@@ -5,92 +5,138 @@
 ## Desc: create dataset of proportion of seeds abscised per trap per sp per year
 ## Date created: 2020-08-05
 
-library("tidyverse")
+library("tidyverse") # v 1.3.1
+library("here") # v 1.0.1
 
 # seed rain data
-seedRain <- read.table(here::here("data", "raw",
-	"BCI_TRAP200_20190215_spcorrected.txt"),
-	header=TRUE, stringsAsFactors = FALSE)
+read.table(
+  here::here("data", "raw", "BCI_TRAP200_20190215_spcorrected.txt"),
+  header = TRUE,
+  stringsAsFactors = FALSE
+) -> seed_rain
 
-# seed traits from joe
-seedTrait <- read.csv(here:here("data", "raw",
-	"20120227_seedsMassForTraits.csv"),
-	header=TRUE, stringsAsFactors = FALSE)
+# seed traits from Joe
+read.csv(
+  here::here("data", "raw", "20120227_seedsMassForTraits.csv"),
+  header = TRUE,
+  stringsAsFactors = FALSE
+) %>%
+  select(LIFEFORM, N_SEEDFULL, SP4) -> seed_trait
 
-# trap locations
-load(here:here("data", "clean", "trapLocations.RData"))
+read.csv(here::here("data", "clean", "species_list.csv")) -> species_list
 
-## sort out names
-seedRain <- rename(seedRain, SP4 = sp)
+read.csv(here::here("data", "clean", "trap_locations.csv")) -> trap_locs
 
-seedTrait <- seedTrait %>%
-   select(LIFEFORM, N_SEEDFULL, SP4, GENUS, SPECIES)
+# clean seed rain data ----------------------------------------------------
 
-seedRain$fecha <- as.character(seedRain$fecha)
-seedRain$fecha <- as.Date(seedRain$fecha, "%Y-%m-%d")
-seedRain$year <- format(as.Date(seedRain$fecha), "%Y")
+seed_rain %>%
+  mutate(fecha = as.character(fecha)) %>%
+  mutate(fecha = as.Date(fecha, "%Y-%m-%d"),
+         year = format(as.Date(fecha), "%Y")) %>%
+  left_join(seed_trait, by = c("sp" = "SP4")) %>%
 
-seedDat <- left_join(seedRain, seedTrait, by = c("SP4"))
+  # don't have the full data for these years
+  filter(year != "1987" & year != "2019") %>%
 
-seedDat <- subset(seedDat, part == 1|part == 2|part == 5)
+  # keep only woody stems
+  filter(
+    LIFEFORM == "SHRUB" |
+      LIFEFORM == "UNDERSTORY" |
+      LIFEFORM == "MIDSTORY" | LIFEFORM == "TREE"
+  ) %>%
+  drop_na("N_SEEDFULL") %>%
+  mutate(sp = tolower(sp),
+         trap = formatC(
+           trap,
+           width = 3,
+           format = "d",
+           flag = "0"
+         )) %>%
+  mutate(trap = paste("trap", trap, sep = "_")) %>%
+  rename(sp4 = sp, n_seedfull = N_SEEDFULL) -> seed_dat
 
-seedDat <- subset(seedDat, LIFEFORM != "EPIPHYTE"|LIFEFORM != "HEMIEPIPHYTE"|LIFEFORM != "HERB"|LIFEFORM != "VINE")
+# sum weekly counts of parts into yearly counts per trap per species
+seed_dat %>%
+  filter(part == 1 | part == 2 | part == 5 | part == 3) %>%
+  group_by(part, sp4, year, trap, n_seedfull) %>%
+  summarise(quantity_sum = sum(quantity, na.rm = TRUE),
+            .groups = "drop") -> abs_dat
 
-seedDat <- seedDat %>%
-	drop_na("N_SEEDFULL")
+# combine with species data to drop species that we can't use
+species_list %>%
+  filter(dioecious == FALSE &
+           ((animal_disp == FALSE | is.na(animal_disp)) | capsules == TRUE)) %>%
+  select("sp4", "capsules", "capsules_per_fruit") %>%
+  inner_join(abs_dat, by = "sp4") -> abs_dat_comb
 
-# don't have the full data for these years
-seedDat <- subset(seedDat, seedDat$year != "1987" & seedDat$year != "2019")
+# calculate viable seeds --------------------------------------------------
 
-# format trap id
-seedDat$trap <- formatC(seedDat$trap, width = 3, format = "d", flag = "0")
-seedDat$trap <- paste("trap", seedDat$trap, sep="_")
+abs_dat_comb %>%
+  subset(part == 1 | part == 2 | part == 3) %>%
 
-absdat <- seedDat %>%
-	group_by(part, SP4, year, trap, N_SEEDFULL, GENUS, SPECIES) %>%
-	summarise(quantity_sum= sum(quantity, na.rm = TRUE)) %>%
-	ungroup()
+  rowwise() %>%
+  mutate(parts_avgseeds = case_when(
 
-# Viable seeds
+    # mature fruit
+    part == 1 ~ quantity_sum * n_seedfull,
 
-absdat_v <- subset(absdat, part == 1 | part == 2) %>%
-	replace(., is.na(.), 0) %>%
-	rowwise() %>%
-	mutate(viable_seeds = ifelse(part==1, quantity_sum*N_SEEDFULL, quantity_sum)) %>%
-	ungroup() %>%
-	group_by(SP4, year, trap, GENUS, SPECIES) %>%
-	summarise(viable_seeds= sum(viable_seeds, na.rm = TRUE)) %>%
-	ungroup()
+    # single diaspores
+    part == 2 ~ quantity_sum,
 
-# Abscised seeds
+    # capsules
+    part == 3 ~ (quantity_sum / capsules_per_fruit) * n_seedfull)) %>%
 
-absdat_a <- subset(absdat, part == 5) %>%
-	replace(., is.na(.), 0) %>%
-	rowwise() %>%
-	mutate(abscised_seeds = quantity_sum*N_SEEDFULL) %>%
-	ungroup() %>%
-	group_by(SP4, year, trap,GENUS, SPECIES) %>%
-	summarise(abscised_seeds= sum(abscised_seeds, na.rm = TRUE)) %>%
-	ungroup()
+  # estimate number of mature fruits based on fruits + capsules, and based on fruits + seeds
+  group_by(sp4, year, trap, capsules) %>%
+  summarise(capsules_seeds = ifelse(
+    part == 1 | part == 3, sum(parts_avgseeds, na.rm = TRUE), 0),
 
-# join and calculate proportion abcised
-propdat <- full_join(absdat_a, absdat_v, by= c("SP4", "year", "trap", "GENUS", "SPECIES"))
+    fruits_seeds = ifelse(
+      part == 1 | part == 2, sum(parts_avgseeds, na.rm = TRUE), 0),
+    .groups = "drop") %>%
 
-propdat <- propdat %>%
-	replace(., is.na(.), 0) %>%
-	rowwise() %>%
-	mutate(total_seeds = sum(abscised_seeds, viable_seeds, na.rm = TRUE), proportion_abscised = abscised_seeds / total_seeds) %>%
-	ungroup()
+  # if fruits + seeds estimate is larger, use that rather than fruits + capsules
+  mutate(capsules_seeds =
+           ifelse(fruits_seeds > capsules_seeds, fruits_seeds, capsules_seeds)) %>%
 
-sumdat <- absdat %>%
-	group_by(SP4, year, trap) %>%
-	summarise(sum_parts= sum(quantity_sum, na.rm = TRUE)) %>%
-	ungroup()
+  # if we can't estimate fruits from capsules for a species, use fruits + seeds
+  mutate(viable_seeds = case_when(
+    capsules == FALSE | is.na(capsules) ~ fruits_seeds,
+    capsules == TRUE ~ capsules_seeds)) %>%
 
-propdat_loc <- left_join(trap_loc_quad, propdat, by = c("trap" = "trap"))
+  select(-capsules_seeds, -fruits_seeds) -> abs_dat_viable
 
-propdat_loc %>%
-	left_join(sumdat, by = c("SP4", "year", "trap")) %>%
-	filter(!sum_parts == 0) -> trapDat
+# calculate abscised seeds ------------------------------------------------
 
-save(trapDat, file = here::here("data", "clean", "trapData.RData"))
+# abscised seeds,  part 5 = immature fruit
+abs_dat_comb %>%
+  subset(part == 5) %>%
+  rowwise() %>%
+  mutate(abscised_seeds = quantity_sum*n_seedfull) %>%
+  group_by(sp4, year, trap) %>%
+  summarise(abscised_seeds = sum(abscised_seeds, na.rm = TRUE),
+            .groups = "drop") -> abs_dat_abscised
+
+# calculate proportion abscised -------------------------------------------
+
+# join abscised and viable seeds data, where there is no match will = NA, change to zero
+full_join(abs_dat_abscised, abs_dat_viable, by= c("sp4", "year", "trap")) %>%
+  mutate_at(vars(viable_seeds, abscised_seeds), ~replace(., is.na(.), 0)) -> abs_dat_abscised_viable
+
+# calculate proportion abscised
+abs_dat_abscised_viable %>%
+  rowwise() %>%
+  mutate(total_seeds = sum(abscised_seeds, viable_seeds, na.rm = TRUE),
+         proportion_abscised = abscised_seeds / total_seeds) %>%
+  ungroup() -> prop_dat
+
+abs_dat %>%
+  group_by(sp4, year, trap) %>%
+  summarise(sum_parts = sum(quantity_sum, na.rm = TRUE), .groups = "drop") -> sum_dat
+
+prop_dat %>%
+  left_join(sum_dat, by = c("sp4", "year", "trap")) %>%
+  left_join(trap_locs, by = "trap") -> trap_dat
+
+saveRDS(trap_dat,
+          here::here("data", "clean", "trap_data.rds"))
