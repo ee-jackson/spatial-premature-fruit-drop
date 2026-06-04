d5f17554fcd9414c93b306e64739949c5d257470
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2026-06-04 10:10:51 +0100

    Update 10_fit-model.R

--- code/scripts/10_fit-model.R
+++ code/scripts/10_fit-model.R
@@ -19,6 +19,9 @@ library("extraDistr", lib.loc = "~/local/rlibs")
 test_data <-
   readRDS("data/clean/trap_connect_buffer.rds")
 
+test_data_preds <-
+  readRDS("data/clean/trap_connect_buffer_preds.rds")
+
 
 # Define models -----------------------------------------------------------
 
@@ -53,6 +56,18 @@ binom_mod_abund <-
     family = beta_binomial(link = "logit", link_phi = "log")
   )
 
+binom_mod_abund_preds <-
+  bf(
+    abscised_seeds | trials(total_seeds) ~
+      (conn_RC_sc + conn_RH_sc + conn_NRC_sc) * log_total_seeds_sc +
+      (conn_RC_sc + conn_RH_sc + conn_NRC_sc) * log_median_abundance_sc +
+      (conn_RC_sc + conn_RH_sc + conn_NRC_sc) * SeedPred_pres +
+      (1 | quadrat/trap) +
+      (1 | year) +
+      (1 + conn_RC_sc + conn_RH_sc + conn_NRC_sc | sp4),
+    family = beta_binomial(link = "logit", link_phi = "log")
+  )
+
 
 # Set priors --------------------------------------------------------------
 
@@ -63,45 +78,45 @@ priors <- c(
 
 # Fit model ---------------------------------------------------------------
 
-fit <-
-  brm(
-    formula = binom_mod,
-    data = test_data,
-    prior = priors,
-    sample_prior = "yes",
-    chains = 4,
-    iter = 5000,
-    control = list(adapt_delta = 0.95),
-    cores = 4,
-    seed = 123,
-    init_r = 0.1,
-    file = "output/models/pheno-repro-adjust/full_conn_binom_nseeds"
-  )
-
-brms::add_criterion(x = fit, criterion = "loo",
-                    overwrite = TRUE)
-
-print(fit$criteria$loo)
-
-fit_int <-
-  brm(
-    formula = binom_mod_int,
-    data = test_data,
-    prior = priors,
-    sample_prior = "yes",
-    chains = 4,
-    iter = 5000,
-    control = list(adapt_delta = 0.95),
-    cores = 4,
-    seed = 123,
-    init_r = 0.1,
-    file = "output/models/pheno-repro-adjust/full_conn_binom_nseeds_interact"
-  )
-
-brms::add_criterion(x = fit_int, criterion = "loo",
-                    overwrite = TRUE)
-
-print(fit_int$criteria$loo)
+# fit <-
+#   brm(
+#     formula = binom_mod,
+#     data = test_data,
+#     prior = priors,
+#     sample_prior = "yes",
+#     chains = 4,
+#     iter = 5000,
+#     control = list(adapt_delta = 0.95),
+#     cores = 4,
+#     seed = 123,
+#     init_r = 0.1,
+#     file = "output/models/pheno-repro-adjust/full_conn_binom_nseeds"
+#   )
+#
+# brms::add_criterion(x = fit, criterion = "loo",
+#                     overwrite = FALSE)
+#
+# print(fit$criteria$loo)
+#
+# fit_int <-
+#   brm(
+#     formula = binom_mod_int,
+#     data = test_data,
+#     prior = priors,
+#     sample_prior = "yes",
+#     chains = 4,
+#     iter = 5000,
+#     control = list(adapt_delta = 0.95),
+#     cores = 4,
+#     seed = 123,
+#     init_r = 0.1,
+#     file = "output/models/pheno-repro-adjust/full_conn_binom_nseeds_interact"
+#   )
+#
+# brms::add_criterion(x = fit_int, criterion = "loo",
+#                     overwrite = FALSE)
+#
+# print(fit_int$criteria$loo)
 
 fit_abund <-
   brm(
@@ -115,10 +130,52 @@ fit_abund <-
     cores = 4,
     seed = 123,
     init_r = 0.1,
+    file_refit = "always",
     file = "output/models/pheno-repro-adjust/full_conn_binom_nseeds_abund"
   )
 
-brms::add_criterion(x = fit_abund, criterion = "loo",
-                    overwrite = TRUE)
+brms::add_criterion(x = fit_abund, criterion = "loo")
 
 print(fit_abund$criteria$loo)
+
+# fit_abund <-
+#   brm(
+#     formula = binom_mod_abund,
+#     data = test_data_preds,
+#     prior = priors,
+#     sample_prior = "yes",
+#     chains = 4,
+#     iter = 5000,
+#     control = list(adapt_delta = 0.95),
+#     cores = 4,
+#     seed = 123,
+#     init_r = 0.1,
+#     file_refit = "never",
+#     file = "output/models/pheno-repro-adjust/full_conn_binom_nseeds_abund_predscomp"
+#   )
+#
+# brms::add_criterion(x = fit_abund, criterion = "loo")
+#
+# print(fit_abund$criteria$loo)
+#
+# fit_abund_preds <-
+#   brm(
+#     formula = binom_mod_abund_preds,
+#     data = test_data_preds,
+#     prior = priors,
+#     sample_prior = "yes",
+#     chains = 4,
+#     iter = 5000,
+#     control = list(adapt_delta = 0.95),
+#     cores = 4,
+#     seed = 123,
+#     init_r = 0.1,
+#     file_refit = "never",
+#     file = "output/models/pheno-repro-adjust/full_conn_binom_nseeds_abund_preds"
+#   )
+#
+# brms::add_criterion(x = fit_abund_preds, criterion = "loo")
+#
+# print(fit_abund_preds$criteria$loo)
+#
+# loo_compare(fit_abund, fit_abund_preds)
