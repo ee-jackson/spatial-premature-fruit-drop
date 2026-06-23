a39136a93bd2c1c6cba0f2b13331d6c94edc96fa
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2026-06-23 10:40:22 +0100

    Update 13_make-fig-02.R

--- code/scripts/13_make-fig-02.R
+++ code/scripts/13_make-fig-02.R
@@ -201,8 +201,8 @@ make_plot <- function(data, effect, x, x_name, mid_colour) {
                     fill = as.factor(effect2__)), alpha = 0.3) +
     geom_line(aes(colour = as.factor(effect2__)), linewidth = 0.5) +
     labs(x = x_name, y = "Immature seed mortality",
-         colour = "Total conspecific seeds",
-         fill = "Total conspecific seeds") +
+         colour = "Total conspecific\nseeds",
+         fill = "Total conspecific\nseeds") +
     scale_colour_manual(
       aesthetics = c("colour", "fill"),
       values = vals
